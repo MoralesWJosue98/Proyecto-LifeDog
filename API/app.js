f1b4543edce2d36d6cceb23741605b06a76e4da7
NNieto25 <00199019@uca.edu.sv>
2021-06-30 22:19:59 -0600

    Modificación de la API Se definen las subcarpetas de modelos, controladores, utils, asi como la implementación de la configuración de la base de datos.
    Authored by: Nestor Nieto <00199019@uca.edu.sv>

--- API/app.js
+++ API/app.js
@@ -6,6 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ApiRouter = require('./routes/api/index');
+
+
+var database = require('./database/mongodb');
+
+database.connect();
 
 var app = express();
 
@@ -21,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', ApiRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ API/routes/api/user.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const {createUser, loginUser, getAll} = require('../../controllers/UserController');
+
+/* GET home page. */
+router.post('/', createUser);
+
+router.get('/', getAll);
+
+router.post('/login', loginUser);
+
+module.exports = router;
