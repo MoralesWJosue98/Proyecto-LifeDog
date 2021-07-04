9ba8ea49934237d881ec1ab8ca76155f02c43e93
Carolina Carranza <cmcc1505@gmail.com>
2021-07-04 02:45:24 -0600

    Implementación de distintas vistas de la aplicación Se agregaron las vistas de seleccionar el perfil del perro, medicamentos, desparasitación, paseos, cortes de pelo, consultas y el menú de gastos. También se modificaron algunos archivos de strings y dimensiones para una vista más organizada.
    Authored by: Carolina Carranza <00008119@uca.edu.sv>

---
+++ LifeDogApp/app/src/main/java/com/carolinac/lifedogapp/ui/ExpensesFragment.kt
@@ -0,0 +1,62 @@
+package com.carolinac.lifedogapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.navigation.findNavController
+import com.carolinac.lifedogapp.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ExpensesFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ExpensesFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var spinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            //param1 = it.getString(ARG_PARAM1)
+            //param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expenses, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Verificar que funcione
+        fillSpinner(view)
+    }
+
+    private fun fillSpinner(view : View){
+        spinner = view.findViewById(R.id.statistics_time_spinner)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.statistics_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+}
+
