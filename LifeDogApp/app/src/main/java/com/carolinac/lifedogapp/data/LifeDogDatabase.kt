9935d29a32ae9c4a075b1f39d1051d9b73baa55d
Wilfredo Josué <00025719@uca.edu.sv>
2021-07-07 03:41:26 -0600

    Add Dog Tips
    Necessary tips have been added, depending on the size of the dog.

--- LifeDogApp/app/src/main/java/com/carolinac/lifedogapp/data/LifeDogDatabase.kt
+++ LifeDogApp/app/src/main/java/com/carolinac/lifedogapp/data/LifeDogDatabase.kt
@@ -12,7 +12,8 @@ import java.util.concurrent.Executors
         Consultation::class, DewormerType::class, Deworming::class, Disease::class, Dog::class, Feeding::class,
         FoodCategory::class, FoodExpense::class, Haircut::class, Medication::class, Medicine::class,
         MedicineCategory::class, MedicineExpense::class, Size::class, Term::class, User::class,
-        UserXDog::class, Vaccine::class, VaccineCategory::class, VaccineExpense::class, DogWalk::class],
+        UserXDog::class, Vaccine::class, VaccineCategory::class, VaccineExpense::class, DogWalk::class,
+        Advices::class],
     version = 1
 )
 @TypeConverters(Converters::class, PriceConverters::class)
@@ -55,13 +56,22 @@ abstract class LifeDogDatabase : RoomDatabase() {
                                 .insertOrUpdateSize(PREPOPULATE_SIZE)
                             /* Food Category */
                             getDatabase(context).foodDao()
-                                .insertOrUpdateFoodCategory(PREPOPULATE_FOODCATEGORY)
+                                .insertOrUpdateFoodCategory(PREPOPULATE_FOOD_CATEGORY)
                             /* Vaccine Category */
                             getDatabase(context).vaccineDao()
-                                .insertOrUpdateVaccineCategory(PREPOPULATE_VACCINECATEGORY)
+                                .insertOrUpdateVaccineCategory(PREPOPULATE_VACCINE_CATEGORY)
                             /* Medicine Category*/
                             getDatabase(context).healthDao()
-                                .insertOrUpdateMedicineCategory(PREPOPULATE_MEDICINECATEGORY)
+                                .insertOrUpdateMedicineCategory(PREPOPULATE_MEDICINE_CATEGORY)
+                            /* Consejos para Razas Pequeñas */
+                            getDatabase(context).dogDao()
+                                .insertAdviceSmall(PREPOPULATE_ADVICE_SMALL)
+                            /* Consejos para Razas Medianas */
+                            getDatabase(context).dogDao()
+                                .insertAdviceMedium(PREPOPULATE_ADVICE_MEDIUM)
+                            /* Consejos para Razas Grandes */
+                            getDatabase(context).dogDao()
+                                .insertAdviceBig(PREPOPULATE_ADVICE_BIG)
                         }
                     }
 
@@ -73,19 +83,23 @@ abstract class LifeDogDatabase : RoomDatabase() {
 
         val PREPOPULATE_DEWTYPE = listOf(
             DewormerType(1, "Sentry HC WormX Plus "),
-            DewormerType(2, " Beaphar"), DewormerType(3, "Triple Wormer Broad Spectrum")
+            DewormerType(2, " Beaphar"),
+            DewormerType(3, "Triple Wormer Broad Spectrum")
         )
 
         val PREPOPULATE_SIZE = listOf(
-            Size(1, "Pequeño"), Size(2, "Mediano"), Size(3, "Grande")
+            Size(1, "Pequeño"),
+            Size(2, "Mediano"),
+            Size(3, "Grande")
         )
 
-        val PREPOPULATE_FOODCATEGORY = listOf(
-            FoodCategory(1, "Croquetas"), FoodCategory(2, "Galletas"),
+        val PREPOPULATE_FOOD_CATEGORY = listOf(
+            FoodCategory(1, "Croquetas"),
+            FoodCategory(2, "Galletas"),
             FoodCategory(3, "Concentrado")
         )
 
-        val PREPOPULATE_VACCINECATEGORY = listOf(
+        val PREPOPULATE_VACCINE_CATEGORY = listOf(
             VaccineCategory(1, "Parvovirus", "Vacuna contra virus del Moquillo"),
             VaccineCategory(
                 2,
@@ -95,11 +109,73 @@ abstract class LifeDogDatabase : RoomDatabase() {
             VaccineCategory(3, "Rabia", "Vacuna contra virus de la Rabia")
         )
 
-        val PREPOPULATE_MEDICINECATEGORY = listOf(
+        val PREPOPULATE_MEDICINE_CATEGORY = listOf(
             MedicineCategory(1, "AntiPulgas"),
             MedicineCategory(2, "Rimadyl Anti Inflamatorio"),
             MedicineCategory(3, "Dermil Antihistamínico"),
         )
 
+        val PREPOPULATE_ADVICE_SMALL = listOf(
+            Advices(
+                1,
+                1,
+                "Aliméntalo según sus necesidades!, el metabolismo de los canes de baja estatura es muchísimo más acelerado que el resto.",
+                "CiudadDeMascotas.com"
+            ),
+            Advices(
+                2,
+                1,
+                "Cuida sus uñas!, los perros de raza pequeña necesitan que sus uñas sean cortadas con más frecuencia, ya que tienen menos posibilidades de desgastarlas naturalmente que sus congéneres de mayor tamaño.",
+                "misanimales.com"
+            ),
+            Advices(
+                3,
+                1,
+                "Acude periódicamente al veterinario!, las válvulas del corazón pueden ocasionarle problemas a alrededor del 60% de los perros de raza pequeña, haciendo que este órgano se debilite y no bombee la suficiente sangre.",
+                "purina.com"
+            )
+        )
+
+        val PREPOPULATE_ADVICE_MEDIUM = listOf(
+            Advices(
+                4,
+                2,
+                "Alimentación!, toma en cuenta, que por el tamaño de tú perro, su alimentación conllevara porciones más grandes, alimentalo según su tamaño.",
+                "purina.com"
+            ),
+            Advices(
+                5,
+                2,
+                "Tiempo para su paseo!, tú perro tendrá mucha energía, debes de programar sus paseos necesarios y los tiempos de juego.",
+                "educachorros.com"
+            ),
+            Advices(
+                6,
+                2,
+                "Bañalo!, tú perro requiere de baños mínimo quincenales, cepillalo muy bien y ten en cuenta los cuidados básicos para una buena higiene.",
+                "adiestramientocanino.com"
+            )
+        )
+
+        val PREPOPULATE_ADVICE_BIG = listOf(
+            Advices(
+                7,
+                3,
+                "Espacio!, tú perro necesita tener el espacio suficiente como para estar cómodo, y sentirse feliz",
+                "kiwoko.com"
+            ),
+            Advices(
+                8,
+                3,
+                "Socializa!, tú perro tiene que socializar con otros perros y humanos para que se siente cómodo frente a situaciones que representen mucho estrés.",
+                "misanimales.com"
+            ),
+            Advices(
+                9,
+                3,
+                "Acude al Veterinario!, los perros de razas grandes son propensos a ciertos problemas de salud, sobre todo relacionados a las articulaciones",
+                "purina.com"
+            )
+        )
     }
 }
