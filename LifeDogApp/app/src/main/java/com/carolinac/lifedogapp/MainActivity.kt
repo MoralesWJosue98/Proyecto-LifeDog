161187edecf4766f89af811c1cb09927217ceaeb
Wilfredo Josué <00025719@uca.edu.sv>
2021-07-07 02:34:48 -0600

    Add Prepopulate Data
    Added autocomplete data allowing you to choose a variety of options

--- LifeDogApp/app/src/main/java/com/carolinac/lifedogapp/MainActivity.kt
+++ LifeDogApp/app/src/main/java/com/carolinac/lifedogapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.carolinac.lifedogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.carolinac.lifedogapp.repository.LifeDogRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
